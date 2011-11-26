a573c0bcdb76588e7bd3acc201f3ae03e6571787
Jarrod Baumann <jarrod@unixc.org>
2011-11-26 03:17:42 -0600

    removed temporary lines that prevented production from displaying

--- views/index.py
+++ views/index.py
@@ -2,6 +2,5 @@ from lib.base import BaseHandler
 
 class IndexHandler(BaseHandler):
     def get(self):
-        a = self.conn.Comic.find_one()
-        self.render("index.html", comic=a)
+        self.render("index.html")
 
